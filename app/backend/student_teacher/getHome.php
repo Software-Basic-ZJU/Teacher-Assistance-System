d21512c958cd6db49bf778d5ed4a013820bf405e
Zhiqiang Wu <wu-zhiqiang@hotmail.com>
2016-11-21 16:47:11 +0800

    update getHome.php

--- app/backend/student_teacher/getHome.php
+++ app/backend/student_teacher/getHome.php
@@ -15,6 +15,7 @@ connectDB();
 loginCheck($_POST['token']);
 //Get information
 $class_id = test_input(mysqli_escape_string($conn, $_POST['class_id']));
+//select *, count(distinct name) from table group by name
 $query_result = mysqli_query($conn, "select DISTINCT tname,phone,email,wechat,qq,other_contact from class_teacher JOIN teacher
                                          where class_id ='$class_id'");
 if($fetched = mysqli_fetch_array($query_result)){
