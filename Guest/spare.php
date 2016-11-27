532286125098fd801f3204c7f1b145d1c9c1ad42
Isacclee <447473281@qq.com>
2016-11-28 00:27:54 +0800

    主页及跳转完成

---
+++ Guest/spare.php
@@ -0,0 +1,378 @@
+<!--左右小圆点滚动元素-->
+<section class="tabsblock">
+    <div class="wrap">
+        <div class="tab">
+            <ul class="tabs clearfix">
+                <li class="active"></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul><!-- tabs -->
+            <!--					国际国内背景	-->
+            <div class="box visible">
+                <div class="box-text">
+                    <h3>全国最先进的教学体验</h3>
+                    <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit aliquet felis, quis ultrices orci condiment.</p>
+                    <img src="images/tabs.png" width="200" height="200" alt="">
+                </div>
+            </div><!-- box -->
+            <div class="box">
+                <div class="box-text">
+                    <h3>The easiest way to show off your best apps</h3>
+                    <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit aliquet felis, quis ultrices orci condiment.</p>
+                </div>
+                <img src="images/tabs.png" width="437" height="459" alt="">
+            </div><!-- box -->
+            <div class="box">
+                <div class="box-text">
+                    <h3>The easiest way to show off your best apps</h3>
+                    <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit aliquet felis, quis ultrices orci condiment.</p>
+                </div>
+                <img src="images/tabs.png" width="437" height="459" alt="">
+            </div><!-- box -->
+            <div class="box">
+                <div class="box-text">
+                    <h3>The easiest way to show off your best apps</h3>
+                    <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit aliquet felis, quis ultrices orci condiment.</p>
+                </div>
+                <img src="images/tabs.png" width="437" height="459" alt="">
+            </div><!-- box -->
+        </div><!-- tab -->
+    </div><!-- wrap -->
+</section><!--  -->
+
+<!--(⊙o⊙)…一种呈现方式-->
+<section id="?" class="box-content box-1">
+    <div class="container">
+        <div class="row heading">
+            <div class="col-lg-12">
+                <h2>最新动态</h2>
+                <hr>
+                <div class="intro"><a href="newList.php">更多动态</a> </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-3 box-item">
+                <div class="wrap-img">
+                    <img src="images/Html.png" />
+                </div>
+                <h3 class="blue">**会议感悟</h3>
+                <p>网易MOOC知名老师,开有诸多慕课,本校中教学Java,C,OOP和操作系统,体系等课程</p>
+                <button type="submit" class="btn btn-2 " onclick="{location.href='Newcontent.php'}" value="?" >detail</button>
+            </div>
+            <!--					<div class="col-sm-3 box-item">-->
+            <!--						<div class="wrap-img">-->
+            <!--							<img src="images/Css.png" />-->
+            <!--						</div>-->
+            <!--						<h3 class="yellow">Text Heading 3</h3>-->
+            <!--						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.-->
+            <!--						Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>-->
+            <!--						<button type="submit" class="btn btn-2 ">More</button>-->
+            <!--					</div>-->
+            <!--					<div class="col-sm-3 box-item">-->
+            <!--						<div class="wrap-img">-->
+            <!--							<img src="images/screen.png" />-->
+            <!--						</div>-->
+            <!--						<h3 class="red">Text Heading 3</h3>-->
+            <!--						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. -->
+            <!--						Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>-->
+            <!--						<button type="submit" class="btn btn-2 ">More</button>-->
+            <!--					</div>-->
+            <!--					<div class="col-sm-3 box-item">-->
+            <!--						<div class="wrap-img">-->
+            <!--							<img src="images/Setting.png" />-->
+            <!--						</div>-->
+            <!--						<h3 class="green">Text Heading 3</h3>-->
+            <!--						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.-->
+            <!--						Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>-->
+            <!--						<button type="submit" class="btn btn-2 ">More</button>-->
+            <!--					</div>-->
+        </div>
+    </div>
+</section>
+
+
+<!--时间轴 -->
+<section id="about" class="box-content box-3">
+	<div class="container">
+		<div class="row heading">
+			<div class="col-lg-12">
+				<h2>About</h2>
+				<hr>
+				<div class="intro">Lorem ipsum dolor sit amet</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<ul class="timeline">
+					<li class="blue">
+						<div class="timeline-image">
+							<img class="img-circle img-responsive" src="images/4.jpg" alt="">
+						</div>
+						<div class="timeline-panel">
+							<div class="timeline-heading">
+								<h4>2009-2011</h4>
+								<h4 class="subheading">Our Humble Beginnings</h4>
+							</div>
+							<div class="timeline-body">
+								<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+							</div>
+						</div>
+					</li>
+					<li class="timeline-inverted red">
+						<div class="timeline-image">
+							<img class="img-circle img-responsive" src="images/5.jpg" alt="">
+						</div>
+						<div class="timeline-panel">
+							<div class="timeline-heading">
+								<h4>March 2011</h4>
+								<h4 class="subheading">An Agency is Born</h4>
+							</div>
+							<div class="timeline-body">
+								<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+							</div>
+						</div>
+					</li>
+					<li class="green">
+						<div class="timeline-image">
+							<img class="img-circle img-responsive" src="images/6.jpg" alt="">
+						</div>
+						<div class="timeline-panel">
+							<div class="timeline-heading">
+								<h4>December 2012</h4>
+								<h4 class="subheading">Transition to Full Service</h4>
+							</div>
+							<div class="timeline-body">
+								<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+							</div>
+						</div>
+					</li>
+					<li class="timeline-inverted yellow">
+						<div class="timeline-image">
+							<img class="img-circle img-responsive" src="images/7.jpg" alt="">
+						</div>
+						<div class="timeline-panel">
+							<div class="timeline-heading">
+								<h4>July 2014</h4>
+								<h4 class="subheading">Phase Two Expansion</h4>
+							</div>
+							<div class="timeline-body">
+								<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+							</div>
+						</div>
+					</li>
+					<li class="timeline-inverted ">
+						<div class="timeline-image">
+							<h4>Be Part
+							<br>Of Our
+							<br>Story!</h4>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- 留言板 -->
+<section class="box-content box-5" id="contact">
+    <div class="container">
+        <div class="row heading">
+            <div class="col-lg-12">
+                <h2>Contact</h2>
+                <hr>
+                <div class="intro">Lorem ipsum dolor sit amet</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 ">
+                <div class="row box-item">
+                    <h3>Complete the Submission Form</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
+                    <h3>Or Just Make a Call</h3>
+                    <p>+6221 888 888 90 <br>
+                        +6221 888 88891</p>
+                    <p>info@yourdomain.com</p>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div id="contact-form">
+                    <form name="form1" method="post" action="contact.php">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control input-lg" name="name" id="name" placeholder="Enter name" required="required" />
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="email" class="form-control input-lg" name="email" id="email" placeholder="Enter email" required="required" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control input-lg" name="subject" id="subject" placeholder="Subject" required="required" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+											<textarea name="message" id="message" class="form-control" rows="4" cols="25" required="required"
+                                                      placeholder="Message"></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-3 btn-block" name="btnContactUs" id="btnContactUs">
+                                    Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- See all的套路-->
+<div class="cd-see-all"><a href="#0" class="btn btn-1">See all</a></div>
+<div class="cd-testimonials-all">
+    <div class="cd-testimonials-all-wrapper">
+        <ul>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsuweqrewqr23143214weqrweqm dolor sit amet, consectetur adipisicing elit. Odit totam saepe iste maiores neque animi molestias nihil illum nisi temporibus.</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-1.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore nostrum nisi, doloremque error hic nam nemo doloribus porro impedit perferendis. Tempora, distinctio hic suscipit. At ullam eaque atque recusandae modi fugiat voluptatem laborum laboriosam rerum, consequatur reprehenderit omnis, enim pariatur nam, quidem, quas vel reiciendis aspernatur consequuntur. Commodi quasi enim, nisi alias fugit architecto, doloremque, eligendi quam autem exercitationem consectetur.</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-2.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem quibusdam eveniet, molestiae laborum voluptatibus minima hic quasi accusamus ut facere, eius expedita, voluptatem? Repellat incidunt veniam quaerat, qui laboriosam dicta. Quidem ducimus laudantium dolorum enim qui at ipsum, a error.</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-3.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero voluptates officiis tempore quae officia! Beatae quia deleniti cum corporis eos perferendis libero reiciendis nemo iusto accusamus, debitis tempora voluptas praesentium repudiandae laboriosam excepturi laborum, nisi optio repellat explicabo, incidunt ex numquam. Ullam perferendis officiis harum doloribus quae corrupti minima quia, aliquam nostrum expedita pariatur maxime repellat, voluptas sunt unde, inventore.</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-4.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit totam saepe iste maiores neque animi molestias nihil illum nisi temporibus.</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-5.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quia quas, quis illo adipisci voluptate ex harum iste commodi nulla dolor. Eius ratione quod ab!</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-6.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, dignissimos iure rem fugiat consequuntur officiis.</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-1.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At temporibus tempora necessitatibus reiciendis provident deserunt maxime sit id. Dicta aut voluptatibus placeat quibusdam vel, dolore.</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-2.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis iusto sapiente, excepturi velit, beatae possimus est tenetur cumque fugit tempore dolore fugiat! Recusandae, vel suscipit? Perspiciatis non similique sint suscipit officia illo, accusamus dolorum, voluptate vitae quia ea amet optio magni voluptatem nemo, natus nihil.</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-3.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quasi officiis pariatur, fugit minus omnis animi ut assumenda quod commodi, ad a alias maxime unde suscipit magnam, voluptas laboriosam ipsam quibusdam quidem, dolorem deleniti id.</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-4.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At temporibus tempora necessitatibus reiciendis provident deserunt maxime sit id. Dicta aut voluptatibus placeat quibusdam vel, dolore.</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-5.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+            <li class="cd-testimonials-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque tempore ipsam, eos suscipit nostrum molestias reprehenderit, rerum amet cum similique a, ipsum soluta delectus explicabo nihil repellat incidunt! Minima magni possimus mollitia deserunt facere, tempore earum modi, ea ipsa dicta temporibus suscipit quidem ut quibusdam vero voluptatibus nostrum excepturi explicabo nulla harum, molestiae alias. Ab, quidem rem fugit delectus quod.</p>
+
+                <div class="cd-author">
+                    <img src="images/avatar-6.jpg" alt="Author image">
+                    <ul class="cd-author-info">
+                        <li>MyName</li>
+                        <li>CEO, CompanyName</li>
+                    </ul>
+                </div> <!-- cd-author -->
+            </li>
+        </ul>
+    </div>	<!-- cd-testimonials-all-wrapper -->
+    <a href="#0" class="close-btn">Close</a>
+</div> <!-- cd-testimonials-all -->
