603eeb5e2c12cc264510ae5472459be6301e5daa
Isacclee <447473281@qq.com>
2016-12-03 01:19:06 +0800

    留言板

--- Guest/MessageBoard.php
+++ Guest/MessageBoard.php
@@ -18,6 +18,8 @@
 	<link rel="stylesheet" href="css/our-team.css"> <!-- Resource style -->
 	<link rel="stylesheet" href="css/simple-line-icons.css">
 	<link rel="stylesheet" href="css/style2.css">
+	<link rel="stylesheet" href="css/creative.css">
+	<link rel="stylesheet" href="css/creative.min.css">
 	<!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 	
@@ -77,104 +79,75 @@
         </div>
         <!-- /.container-fluid -->
     </nav>
-	<!-- Navigation -->
 
-	<!-- Welcome & Login -->
-	<header id="page-top">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="o-container">
-						<div class="c-slack">
-						  <span class="c-slack__dot c-slack__dot--a"></span>
-						  <span class="c-slack__dot c-slack__dot--b"></span>
-						  <span class="c-slack__dot c-slack__dot--c"></span>
-						  <span class="c-slack__dot c-slack__dot--d"></span>
-						</div>
-					</div>
-					<div class="intro-text">
-						<div class="intro-lead-in">Teaching Assistant System</div>
-						<div class="intro-heading"></div>
-					</div>
-					<a class="btn btn-1 btn-sm" href="login.php">Log In</a>
-<!--					<a class="btn btn-1 btn-sm" href="#team">Our Team</a>-->
+	<div class="container" style="margin-top:150px">
+		<div class="row">
+			<div class="col-lg-4 col-lg-offset-4">
+				<div class="text-center">
+					<h2 class="section-heading">留言板</h2>
+					<hr class="light">
 				</div>
 			</div>
 		</div>
-    </header>
-	<!-- Welcome & Login -->
-
-	<!-- Main Part-->
-	
-	<div id="page-content" class="index-page">
-		<!--New 最新动态-->
-		<div id="fh5co-services-section">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
-						<i class="sl-icon-paper-plane"></i>
-						<h2>最新动态</h2>
-						<p><a href="NewList.php"> 相关文章,顶尖会议,知名论文</a></p>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-4 text-center">
-						<div class="services">
-							<span><i class="sl-icon-graph"></i></span>
-							<h3>Dashboard</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-							<div class="cd-see-all"><a href="NewPassage.php" class="btn btn-1">See all</a></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- 最新动态 -->
-
-		<!-- Classes课程信息 -->
-		<section id="Classes" class="box-content box-2 box-style">
-			<div class="container">
-				<div class="row">
-					<blockquote><a href="Classes.php">这是坠吼的课程</a> </blockquote>
-				</div>
-			</div>
-		</section>
-		<!--课程信息-->
-
-
-		<!-- Teachers 教师信息 -->
-		<section id="Teachers" class="box-content box-4 box-style">
-			<div class="clearfix">
-				<div class="cd-testimonials-wrapper cd-container">
-					<ul class="cd-testimonials">
-						<!--人物信息单元-->
-						<li>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-							<div class="cd-author">
-								<img src="images/avatar-1.jpg" alt="Author image">
-								<ul class="cd-author-info">
-									<li>MyName</li>
-									<li>CEO, AmberCreative</li>
-								</ul>
-							</div>
-						</li>
-						<li>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-							<div class="cd-author">
-								<img src="images/avatar-1.jpg" alt="Author image">
-								<ul class="cd-author-info">
-									<li>僵硬</li>
-									<li>坠棒的</li>
-								</ul>
-							</div>
-						</li>
-					</ul> <!-- cd-testimonials -->	
-				</div> <!-- cd-testimonials-wrapper -->
-				<div class="cd-see-all"><a href="TeacherList.php" class="btn btn-1">See all</a></div>
-			</div>
-		</section>
 	</div>
-
+	<div class="container" style="margin-top: 10px">
+		<div class="well well-lg sr-button">
+			<div class="row text-center">
+				<div class="col-sm-4">
+					<p class="text-success">留言标题
+					<p>时间</p>
+				</div>
+				<div class="col-sm-1" style="">
+					<p class="bg-form">内容</p>
+					<a href="#reply" class="btn btn-info" data-toggle="collapse">回复</a>
+				</div>
+			</div>
+			<div id="reply" class="collapse">
+				<form role="form" name="comment" action="?.php" method="post">
+					<div class="row">
+						<div class="col-md">
+							<div class="form-group">
+								<label for="comment">回复:</label>
+								<textarea class="form-control"　name="comment" rows="5" id="comment"></textarea>
+							</div>
+							<div class="col-lg-12 text-center">
+								<button type="submit" name="commentSubmit" class="btn btn-default btn-xl sr-button" style="background-color:#F05F40">提交</button>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="container" style="margin-top: 10px">
+		<div class="well well-lg sr-button">
+			<div class="row text-center">
+				<div class="col-sm-4">
+					<p class="text-success">留言标题
+					<p>时间</p>
+				</div>
+				<div class="col-sm-1" style="">
+					<p class="bg-form">内容</p>
+					<a href="#reply" class="btn btn-info" data-toggle="collapse">回复</a>
+				</div>
+			</div>
+			<div id="reply" class="collapse">
+				<form role="form" name="comment" action="?.php" method="post">
+					<div class="row">
+						<div class="col-md">
+							<div class="form-group">
+								<label for="comment">回复:</label>
+								<textarea class="form-control"　name="comment" rows="5" id="comment"></textarea>
+							</div>
+							<div class="col-lg-12 text-center">
+								<button type="submit" name="commentSubmit" class="btn btn-default btn-xl sr-button" style="background-color:#F05F40">提交</button>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 	<!--底-->
 	<footer>
 		<div class="wrap-footer">
@@ -307,6 +280,7 @@
 	<script src="js/jquery.countTo.js"></script>
 	<!-- Main JS (Do not remove) -->
 	<script src="js/main.js"></script>
+
 
 </body>
 </html>
