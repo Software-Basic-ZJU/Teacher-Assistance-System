87be47cc9594e5927fdfe9f8c20c5d023cc4548a
朱八八 <z19964950@gmail.com>
2016-12-04 02:51:48 +0800

    留言板-获取留言
    实现获取留言
    待做：#回复留言
    #发送留言

--- Guest/MessageBoard.php
+++ Guest/MessageBoard.php
@@ -89,65 +89,12 @@
 				</div>
 			</div>
 		</div>
+		<?php
+		include dirname(__FILE__).'/backend/MessageBoard/Get.php';
+		?>
 	</div>
-	<div class="container" style="margin-top: 10px">
-		<div class="well well-lg sr-button">
-			<div class="row text-center">
-				<div class="col-sm-4">
-					<p class="text-success">留言标题
-					<p>时间</p>
-				</div>
-				<div class="col-sm-1" style="">
-					<p class="bg-form">内容</p>
-					<a href="#reply" class="btn btn-info" data-toggle="collapse">回复</a>
-				</div>
-			</div>
-			<div id="reply" class="collapse">
-				<form role="form" name="comment" action="?.php" method="post">
-					<div class="row">
-						<div class="col-md">
-							<div class="form-group">
-								<label for="comment">回复:</label>
-								<textarea class="form-control"　name="comment" rows="5" id="comment"></textarea>
-							</div>
-							<div class="col-lg-12 text-center">
-								<button type="submit" name="commentSubmit" class="btn btn-default btn-xl sr-button" style="background-color:#F05F40">提交</button>
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	<div class="container" style="margin-top: 10px">
-		<div class="well well-lg sr-button">
-			<div class="row text-center">
-				<div class="col-sm-4">
-					<p class="text-success">留言标题
-					<p>时间</p>
-				</div>
-				<div class="col-sm-1" style="">
-					<p class="bg-form">内容</p>
-					<a href="#reply" class="btn btn-info" data-toggle="collapse">回复</a>
-				</div>
-			</div>
-			<div id="reply" class="collapse">
-				<form role="form" name="comment" action="?.php" method="post">
-					<div class="row">
-						<div class="col-md">
-							<div class="form-group">
-								<label for="comment">回复:</label>
-								<textarea class="form-control"　name="comment" rows="5" id="comment"></textarea>
-							</div>
-							<div class="col-lg-12 text-center">
-								<button type="submit" name="commentSubmit" class="btn btn-default btn-xl sr-button" style="background-color:#F05F40">提交</button>
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
+	
+
 	<!--底-->
 	<footer>
 		<div class="wrap-footer">
