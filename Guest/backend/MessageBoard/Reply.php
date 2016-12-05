7e1545a9ae674f38357da479cbc540ea41965990
朱八八 <z19964950@gmail.com>
2016-12-05 23:26:14 +0800

    留言板-实现留言回复
    待做#留言板游客留言

--- Guest/backend/MessageBoard/Reply.php
+++ Guest/backend/MessageBoard/Reply.php
@@ -1,19 +1,22 @@
 <?php
 
-if(isset($_POST['commentSubmit'])){
+
+if(isset($_POST['replySubmit'])){
     if(isset($_POST['msg_id'])){
         $msg_id=$_POST['msg_id'];
-        if(isset($_POST['comment'])){
-            $comment=$_POST['comment'];
+        if(isset($_POST['reply'])){
+            $reply=$_POST['reply'];
         }
-        else echo "<script>alert(\"回复失败!\");</script>";
+        else 
+        	echo "<script>alert(\"回复失败!\");</script>";
     }
 }
-echo $msg_id;
+
 
 include dirname(dirname(__FILE__)).'/connectDB.php';
 global $conn;
 connectDB();
-$reply_message = mysqli_query($conn, "UPDATE message SET reply_content = '$comment' WHERE msg_id = '$msg_id'") or die("更新失败".mysql_error());
+$reply_message = mysqli_query($conn, "UPDATE message SET reply_content = '$reply' WHERE msg_id = '$msg_id'") or die("更新失败".mysql_error());
+echo "<script>alert(\"更新成功!\");history.back(-1)</script>";
 mysqli_close($conn);
 ?>
