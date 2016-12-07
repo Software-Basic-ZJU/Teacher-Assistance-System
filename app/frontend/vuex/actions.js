bd4290bafee1eb8502c6f7960c7dda057649eb22
yyh1102 <234347589@qq.com>
2016-12-07 16:10:46 +0800

    construct vuex

--- app/frontend/vuex/actions.js
+++ app/frontend/vuex/actions.js
@@ -1,4 +1,5 @@
 import router from "../routes";
+import Vue from "vue";
 
 export const editorSubmit=({commit},payload)=>{
     console.log(payload)
@@ -19,6 +20,29 @@ export const editorSubmit=({commit},payload)=>{
 
 export const login=({commit},loginForm)=>{
     //TODO 如果是首次登陆，则跳转至完成信息界面
+    Vue.http("/backend/login.php",
+        {
+            id:loginForm.id,
+            password:loginForm.password,
+            type:loginForm.idenType
+        }
+    ).then((response)=>{
+        let resp=response.body;
+        if(!resp.code){
+
+        }
+        else if(resp.code==1){
+
+        }
+        else if(resp.code==2){
+
+        }
+        else if(resp.code==3){
+
+        }
+    },(response)=>{
+        Vue.$message(response.body.msg);
+    });
     commit('showCompleteInfo',true);
     console.log(loginForm);
 };
