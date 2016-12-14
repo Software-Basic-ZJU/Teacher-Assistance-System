9dfe91decea7e2466bff635c257129fd5ec1b77e
朱八八 <z19964950@gmail.com>
2016-12-14 14:14:18 +0800

    课件-显示
    能根据文件格式显示不同图标了

--- Guest/MessageBoard.php
+++ Guest/MessageBoard.php
@@ -97,62 +97,6 @@
 		</div>
 	</div>
 
-	<div class="container" style="margin-top: 10px;">
-		<div class="well well-lg sr-button">
-			<div class="row text-center">
-				<div class="col-sm-4">
-					<p class="text-success">留言标题
-					<p>时间</p>
-				</div>
-				<div class="col-sm-1" style="">
-					<p class="bg-form">内容</p>
-					<a href="#reply" class="btn btn-info" data-toggle="collapse">回复</a>
-				</div>
-			</div>
-			<div id="reply" class="collapse">
-				<form role="form" name="comment" action="?.php" method="post">
-					<div class="row">
-						<div class="col-md-6">
-							<textarea class="form-control"　name="comment" rows="5" id="comment"></textarea>
-							<div class="col-lg-12 text-center">
-								</br>
-								<button type="submit" name="commentSubmit" class="btn btn-default btn-xl sr-button" style="background-color:#5bc0de">提交</button>
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-
-
-	<div class="container" style="margin-top: 10px;">
-		<div class="well well-lg sr-button">
-			<div class="row text-center">
-				<div class="col-sm-4">
-					<p class="text-success">留言标题
-					<p>时间</p>
-				</div>
-				<div class="col-sm-1" style="">
-					<p class="bg-form">内容</p>
-					<a href="#reply2" class="btn btn-info" data-toggle="collapse">回复</a>
-				</div>
-			</div>
-			<div id="reply2" class="collapse">
-				<form role="form" name="comment" action="?.php" method="post">
-					<div class="row">
-						<div class="col-md-6">
-							<textarea class="form-control"　name="comment" rows="5" id="comment"></textarea>
-							<div class="col-lg-12 text-center">
-								</br>
-								<button type="submit" name="commentSubmit" class="btn btn-default btn-xl sr-button" style="background-color:#5bc0de">提交</button>
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
 
 	<!--留言框-->
 	<section class="box-content box-5" id="contact">
