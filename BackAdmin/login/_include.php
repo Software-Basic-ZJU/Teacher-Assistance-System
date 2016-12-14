9fc871f600132537f5d2b4557aabdb88e61fa8bb
hongyeliang <958520528@qq.com>
2016-12-14 15:57:20 +0800

    basic function completed. by liang and huang

---
+++ BackAdmin/login/_include.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wu
+ * Date: 2016/11/20
+ * Time: 16:40
+ */
+include 'loginCheck.php';
+function test_input($data) {
+    $data = inject_prevent($data);
+    $data = trim($data);//去除用户输入数据中不必要的字符（多余的空格、制表符、换行）
+    $data = stripslashes($data);//删除用户输入数据中的反斜杠（\）
+    $data = htmlspecialchars($data);//把特殊字符转换为 HTML 实体
+    return $data;
+}
+function inject_prevent($Sql_Str) {//自动过滤Sql的注入语句。
+    $check=preg_match('/select|insert|update|delete|\'|\\*|\*|\.\.\/|\.\/|union|into|load_file|outfile/i',$Sql_Str);
+    if ($check) {
+        $Sql_Str = strtr($Sql_Str,array("select"=>" ", 'insert'=>' ', 'update'=>' ', 'delete'=>' '));
+    }
+    return $Sql_Str;
+}
+
+
+?>
+
+
+
