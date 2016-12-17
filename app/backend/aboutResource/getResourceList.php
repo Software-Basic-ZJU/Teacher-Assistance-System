3e0b383fe9069a2fe5b02f6a97f31f9c77c984ba
Zhiqiang Wu <wu-zhiqiang@hotmail.com>
2016-12-17 16:18:16 +0800

    update getResourceList.php

--- app/backend/aboutResource/getResourceList.php
+++ app/backend/aboutResource/getResourceList.php
@@ -15,41 +15,81 @@ connectDB();
 loginCheck($_SERVER['HTTP_X_ACCESS_TOKEN']);
 //Get information
 $teacher_id = test_input(mysqli_escape_string($conn, $_POST['teacher_id']));
-//select * from resource JOIN select  where uploader_id = 10001 and type = 0
-$query_result = mysqli_query($conn, "select * ,teacher.name as uploader_name,resource.name as resource_name 
+$type = test_input(mysqli_escape_string($conn, $_POST['type']));
+if($type == 0) {
+    $query_result = mysqli_query($conn, "select * ,teacher.name as uploader_name,resource.name as resource_name 
                  from resource join teacher on uploader_id = teacher_id and type = 0 and uploader_id = '$teacher_id';");
-if($query_result){
-    $resourceList = array();
-    while($fetched = mysqli_fetch_array($query_result)){
-        $resourceList[] = array(
-            "resource_id" => $fetched['resrc_id'],
-            "name" => $fetched['resource_name'],
-            "path" =>"http://".$_SERVER['HTTP_HOST']."/backend/aboutResource/". $fetched['path'],
-            "upload_time" => $fetched['time'],
-            "uploader_name" => $fetched['uploader_name'],
-            "size" => $fetched['size'],
-            "authority"=>$fetched['authority']
+    if ($query_result) {
+        $resourceList = array();
+        while ($fetched = mysqli_fetch_array($query_result)) {
+            $resourceList[] = array(
+                "resource_id" => $fetched['resrc_id'],
+                "name" => $fetched['resource_name'],
+                "path" => "http://" . $_SERVER['HTTP_HOST'] . "/backend/aboutResource/" . $fetched['path'],
+                "upload_time" => $fetched['time'],
+                "uploader_name" => $fetched['uploader_name'],
+                "size" => $fetched['size'],
+                "authority" => $fetched['authority']
+            );
+        }
+        $result = array(
+            "code" => 0,
+            "msg" => "查找成功",
+            "res" => array(
+                "token" => $_SESSION['token'],
+                "resource_list" => $resourceList
+            )
         );
+        echo json_encode($result);
+    } else {
+        $result = array(
+            "code" => -1,
+            "msg" => "查找失败，teacher_id错误",
+            "res" => array(
+                "token" => $_SESSION['token']
+            )
+        );
+        echo json_encode($result);
     }
-    $result = array(
-        "code" => 0,
-        "msg" => "查找成功",
-        "res" => array(
-            "token" => $_SESSION['token'],
-            "resource_list" => $resourceList
-        )
-    );
-    echo json_encode($result);
+    mysqli_close($conn);
 }
-else{
-    $result = array(
-        "code" => -1,
-        "msg" => "查找失败，teacher_id错误",
-        "res" => array(
-            "token"=>$_SESSION['token']
-        )
-    );
-    echo json_encode($result);
+elseif($type == 1){
+    $query_result = mysqli_query($conn, "select * ,teacher.name as uploader_name,resource.name as resource_name 
+                 from resource join teacher on uploader_id = teacher_id and type = 1 and uploader_id = '$teacher_id';");
+    if ($query_result) {
+        $resourceList = array();
+        while ($fetched = mysqli_fetch_array($query_result)) {
+            $resourceList[] = array(
+                "resource_id" => $fetched['resrc_id'],
+                "name" => $fetched['resource_name'],
+                "path" => "http://" . $_SERVER['HTTP_HOST'] . "/backend/aboutResource/" . $fetched['path'],
+                "upload_time" => $fetched['time'],
+                "uploader_name" => $fetched['uploader_name'],
+                "size" => $fetched['size'],
+                "authority" => $fetched['authority'],
+                "post_id" => $fetched['post_id']
+            );
+        }
+        $result = array(
+            "code" => 0,
+            "msg" => "查找成功",
+            "res" => array(
+                "token" => $_SESSION['token'],
+                "resource_list" => $resourceList
+            )
+        );
+        echo json_encode($result);
+    } else {
+        $result = array(
+            "code" => -1,
+            "msg" => "查找失败，teacher_id错误",
+            "res" => array(
+                "token" => $_SESSION['token']
+            )
+        );
+        echo json_encode($result);
+    }
+    mysqli_close($conn);
 }
-mysqli_close($conn);
+
 ?>
