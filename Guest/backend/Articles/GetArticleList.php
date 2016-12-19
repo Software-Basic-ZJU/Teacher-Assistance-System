6a98b85c103dbfd8226d952ef0ef9d3d3178b3fb
朱八八 <z19964950@gmail.com>
2016-12-19 23:33:34 +0800

    资源列表-文章列表 修复
    现在只显示authority为0的，也就是对游客可见的资源或文章了

--- Guest/backend/Articles/GetArticleList.php
+++ Guest/backend/Articles/GetArticleList.php
@@ -5,8 +5,8 @@ global $conn;
 connectDB();
 
 $Page_size=9;  //一页文章数量
-
-$result=mysqli_query($conn,'select * from article');
+$teacher_upload=0;  //教师上传的文章
+$result=mysqli_query($conn,"SELECT * from article  where authority = '$teacher_upload'");
 $count = mysqli_num_rows($result);
 $page_count = ceil($count/$Page_size);  //总共多少页
 
@@ -24,7 +24,9 @@ else{
 }
 
 $offset=$Page_size*($page-1);  
-$sql="select * from article limit $offset,$Page_size";
+
+
+$sql="SELECT * from article where authority = '$teacher_upload' limit $offset,$Page_size";
 $result=mysqli_query($conn,$sql);
 $col = 1;  //用于判断一行三篇文章
 while ($row=mysqli_fetch_array($result)) {
