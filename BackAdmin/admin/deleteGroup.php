1f05462dd7117d8d1f09dfd7a6dd516d27420b56
hongyeliang <958520528@qq.com>
2016-12-20 11:16:52 +0800

    added function completed. by liang and huang

--- BackAdmin/admin/deleteGroup.php
+++ BackAdmin/admin/deleteGroup.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: hongyeliang
+ * Date: 2016/12/20
+ * Time: 上午10:01
+ */
 
 header('Content-type: application/json');
 session_start();
@@ -10,15 +16,15 @@ connectDB();
 //loginCheck($_POST['token']);
 
 $result=mysqli_query($conn,
-	"DELETE from classes WHERE class_id='" . $_GET['class_id'] . "';");
+    "DELETE from BackAdmin.group WHERE group_id='" . $_GET['group_id'] . "';");
 
 if($result) {
-$result = array (
-	"code"=>"0",
-	"msg"=>"删除成功！");}
+    $result = array (
+        "code"=>"0",
+        "msg"=>"删除成功！");}
 else $result = array (
-	"code"=>"1",
-	"msg"=>"删除失败！");
+    "code"=>"1",
+    "msg"=>"删除失败！");
 
 echo json_encode($result);
 
