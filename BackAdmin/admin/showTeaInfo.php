84dbed801305d15145e69a56fc461408de46e510
huangruitao <hrtao1996@gmail.com>
2016-12-28 00:43:00 +0800

    bugs corrected

--- BackAdmin/admin/showTeaInfo.php
+++ BackAdmin/admin/showTeaInfo.php
@@ -1,6 +1,6 @@
 <?php
-
 header('Content-type: application/json');
+//header("content-Type: text/html; charset=gb2312");
 //session_start();
 // Connect database
 include '../login/_include.php';
@@ -15,30 +15,25 @@ $teacher_id = test_input(mysqli_escape_string($conn, $_POST['teacher_id']));
 //select *, count(distinct name) from table group by name
 $query_result = mysqli_query($conn, "select teacher_info from teacher 
                                         where teacher_id ='$teacher_id'");
-if(!$query_result){
-    $result = array(
-        "code" => 1,
-        "msg" => "查找失败",
-        "res" => null
-    );
-    echo json_encode($result);
-}
-if($query_result) {
-    $fetched = mysqli_fetch_array($query_result);
-    $result = array(
-        "code" => 0,
-        "msg" => "查找成功",
-        "res" => $fetched['teacher_info']
-    );
-    echo json_encode($result);
-}
-else{
-    $result = array(
-        "code" => 1,
-        "msg" => "查找失败",
-        "res" => null
-    );
-    echo json_encode($result);
-}
+
+$fetched = mysqli_fetch_array($query_result);
+//echo $fetched['teacher_info'];
+    if ($fetched['teacher_info']) {
+        $result = array(
+            "code" => 0,
+            "msg" => "查找成功",
+            "res" => $fetched['teacher_info']
+        );
+        //echo $result;
+        echo json_encode($result);
+    } else {
+        $result = array(
+            "code" => 1,
+            "msg" => "查找失败",
+            "res" => null
+        );
+        echo json_encode($result);
+    }
+
 mysqli_close($conn);
 ?>
