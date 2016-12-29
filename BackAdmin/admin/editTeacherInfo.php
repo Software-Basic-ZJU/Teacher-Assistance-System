e6010400fe081b0d89f389fe760305d494191652
huangruitao <hrtao1996@gmail.com>
2016-12-29 22:03:11 +0800

    bugs corrected

--- BackAdmin/admin/editTeacherInfo.php
+++ BackAdmin/admin/editTeacherInfo.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Wu
- * Date: 2016/11/28
- * Time: 09:24
- */
 header('Content-type: application/json');
 //session_start();
 // Connect database
@@ -20,23 +14,27 @@ $teacher_id = test_input(mysqli_escape_string($conn, $_POST['teacher_id']));
 $query_result = mysqli_query($conn, "update teacher
                                      set teacher_info = '$teacher_info'
                                      WHERE teacher_id = '$teacher_id';");
-if($query_result){
-    $result = array(
-        "code" => 0,
-        "msg" => "更改成功",
-        "res" => array(
-            'teacher_info' => $teacher_info
-        )
-    );
-    echo json_encode($result);
-}
-else{
-    $result = array(
-        "code" => 1,
-        "msg" => "更改失败",
-        "res" => null
-    );
-    echo json_encode($result);
-}
+
+$query_result1 = mysqli_query($conn, "select * from teacher
+                                        where teacher_id ='$teacher_id'");
+
+    if ($fetched = mysqli_fetch_array($query_result1)) {
+        $result = array(
+            "code" => 0,
+            "msg" => "更改成功",
+            "res" => array(
+                'teacher_info' => $teacher_info
+            )
+        );
+        echo json_encode($result);
+    } else {
+        $result = array(
+            "code" => 1,
+            "msg" => "更改失败",
+            "res" => null
+        );
+        echo json_encode($result);
+    }
+
 mysqli_close($conn);
 ?>
