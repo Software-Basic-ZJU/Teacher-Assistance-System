1144f21d395b70f8f2c5fef00f34afbdfeb6d338
huangruitao <hrtao1996@gmail.com>
2017-01-02 13:38:28 +0800

    fix bugs

--- BackAdmin/admin/deleteClass.php
+++ BackAdmin/admin/deleteClass.php
@@ -8,12 +8,17 @@ global $conn;
 connectDB();
 //Verify token
 //loginCheck($_POST['token']);
+$class_id = test_input(mysqli_escape_string($conn, $_POST['class_id']));
 
-$result=mysqli_query($conn,
-	"DELETE from classes WHERE class_id='" . $_POST['class_id'] . "';");
+$result1=mysqli_query($conn, "select * from classes
+                                        where class_id ='$class_id'");
 
-if($result) {
-$result = array (
+
+if($fetched = mysqli_fetch_array($result1)) {
+	$result=mysqli_query($conn,
+			"DELETE from classes WHERE class_id=$class_id;");
+
+	$result = array (
 	"code"=>"0",
 	"msg"=>"删除成功！");}
 else $result = array (
